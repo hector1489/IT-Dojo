daecd4f840d7208c03c391cd56afdec44d990433
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-12-15 17:11:52 -0300

    [fix] error-moduleGraphql

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,20 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   </React.StrictMode>
 )
 
-/*const enableMocking = async ():Promise<void>  => {
-  if (process.env.NODE_ENV !== 'development') return
-
-  const { worker } = await import('../mocks/browser.js')
-
-  return worker.start()
-}
-
-enableMocking()
-  .then(() => {
-    ReactDOM.createRoot(document.getElementById('root')!).render(
-      <React.StrictMode>
-          <WrappedApp />
-      </React.StrictMode>,
-    )
-  })
-  .catch(console.error)*/
