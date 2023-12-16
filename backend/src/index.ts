8b5beec21493c7eb295b9585980bcff8a6519df7
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-12-16 09:29:26 -0300

    [fix] Cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,8 @@ const PORT = process.env.PORT ?? 3000
 
 app.use(express.json());
 app.use(cors({
-  origin:  FRONTEND_URL
+  origin: FRONTEND_URL,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 }));
 
 // instancias de modelos
