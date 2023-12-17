5c3e355adde4ab8df72d8e657bc00e28e41e2535
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-12-17 15:58:25 -0300

    [fix] login

--- frontend/src/config/constans.ts
+++ frontend/src/config/constans.ts
@@ -1,9 +1,9 @@
 export const URLBASE = import.meta.env.VITE_BACKEND_URL ?? "HTTP://LOCALHOST:3000";
 
 export const ENDPOINT = {
-  login: `${URLBASE}/users/login`,
-  signup: `${URLBASE}/signup`,
-  users: `${URLBASE}/usuarios`,
+  login: `${URLBASE}/users/login/`,
+  signup: `${URLBASE}/users/signup/`,
+  users: `${URLBASE}/users/`,
   products: `${URLBASE}/inventory/`,
   favorite: `${URLBASE}/favorites/`
 };
